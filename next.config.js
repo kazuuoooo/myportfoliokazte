e8ff9747e2c0a0fdf1fc603d9babd26676cc5611
kazuuoooo <97545410+kazuuoooo@users.noreply.github.com>
2023-02-21 02:11:52 -0800

    first commit

---
+++ next.config.js
@@ -0,0 +1,24 @@
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(pdf)$/,
+      use: [
+        {
+          loader: 'file-loader',
+          options: {
+            publicPath: '/_next/static/files',
+            outputPath: 'static/files/',
+            name: '[name].[ext]',
+            esModule: false,
+          },
+        },
+      ],
+    });
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
